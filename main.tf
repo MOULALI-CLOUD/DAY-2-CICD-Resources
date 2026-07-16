bb6e4a3e97e9c49b1fbb38a90777409bddbc569c
MOULALI <282065718+MOULALI-CLOUD@users.noreply.github.com>
2026-07-16 13:54:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "main" {
 
 resource "aws_instance" "main" {
   ami                         = "ami-01edba92f9036f76e"
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.main.id
   vpc_security_group_ids      = [aws_security_group.main.id]
   associate_public_ip_address = true
